26986eacf333c12c78de346b56dee34fe44af8c6
DashaShevkoplyas <shevkoplyasdarya23@gmail.com>
2023-10-10 14:49:42 +0300

    Refactoring project to multi-model architecture. Closes #1.

--- pet-clinic-web/src/main/java/com/clinic/petclinicspring/PetClinicSpringApplication.java
+++ pet-clinic-web/src/main/java/com/clinic/petclinicspring/PetClinicSpringApplication.java
@@ -1,5 +1,6 @@
 package com.clinic.petclinicspring;
 
+import com.clinic.petclinicspring.models.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
