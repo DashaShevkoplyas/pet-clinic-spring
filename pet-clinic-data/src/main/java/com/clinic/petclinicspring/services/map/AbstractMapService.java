76e849378b62b0f417696cc0e148c79a0ce307cb
DashaShevkoplyas <shevkoplyasdarya23@gmail.com>
2024-01-14 17:55:19 +0300

    Update Map Impl to manage setting of ID property. Closes #23

--- pet-clinic-data/src/main/java/com/clinic/petclinicspring/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinicspring/services/map/AbstractMapService.java
@@ -1,12 +1,20 @@
 package com.clinic.petclinicspring.services.map;
 
-import java.util.*;
+import com.clinic.petclinicspring.models.BaseEntity;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Optional;
+import java.util.Collections;
+import java.util.NoSuchElementException;
 
 /**
  * Base map service.
  */
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -16,8 +24,15 @@ public abstract class AbstractMapService<T, ID> {
         return Optional.ofNullable(map.get(id));
     }
 
-    T save(ID id, T entity) {
-        map.putIfAbsent(id, entity);
+    T save(T entity) {
+        if (entity == null) {
+            throw new RuntimeException("Entity can not be null");
+        }
+
+        if (entity.getId() == null) {
+            entity.setId(getNextId());
+        }
+        map.putIfAbsent(entity.getId(), entity);
         return entity;
     }
 
@@ -27,5 +42,13 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T entity) {
         map.entrySet().removeIf(e -> e.getValue().equals(entity));
+    }
+
+    private Long getNextId() {
+        try {
+            return Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException ex) {
+            return 1L;
+        }
     }
 }
--- pet-clinic-web/src/main/java/com/clinic/petclinicspring/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/clinic/petclinicspring/bootstrap/DataLoader.java
@@ -19,13 +19,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Viona");
         owner1.setLastName("Ridler");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Riddly");
         owner2.setLastName("Hoksmit");
         ownerService.save(owner2);
@@ -33,13 +31,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Oliver");
         vet1.setLastName("Tree");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Daniel");
         vet2.setLastName("Kit");
         vetService.save(vet2);
