6e09376c0ed8f918126004fd901d51c3180b2e5f
DashaShevkoplyas <shevkoplyasdarya23@gmail.com>
2024-01-25 21:32:33 +0300

    Create PetType, Pet and Visit entites. Closes #26.

--- pet-clinic-data/src/main/java/com/clinic/petclinicspring/models/Owner.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinicspring/models/Owner.java
@@ -1,7 +1,18 @@
 package com.clinic.petclinicspring.models;
 
+import java.util.Set;
+
 /**
  * Represents owner of the pet/s.
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
