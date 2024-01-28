27749e73b0f80a029fe038c95c6ea3c792647a17
DashaShevkoplyas <shevkoplyasdarya23@gmail.com>
2024-01-28 15:46:50 +0300

    Add contact info props for owners. Closes #28.

--- pet-clinic-data/src/main/java/com/clinic/petclinicspring/models/Owner.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinicspring/models/Owner.java
@@ -6,7 +6,16 @@ import java.util.Set;
  * Represents owner of the pet/s.
  */
 public class Owner extends Person {
+    private ContactInfo contactInfo;
     private Set<Pet> pets;
+
+    public ContactInfo getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(ContactInfo contactInfo) {
+        this.contactInfo = contactInfo;
+    }
 
     public Set<Pet> getPets() {
         return pets;
