1317a08f23d9dcd4215589532e5b6c88acb3639d
DashaShevkoplyas <shevkoplyasdarya23@gmail.com>
2024-01-28 16:53:10 +0300

    Create specialty map service. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/clinic/petclinicspring/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.clinic.petclinicspring.services;
+
+import com.clinic.petclinicspring.models.Speciality;
+
+/**
+ * Represents speciality service with basic CRUD operations.
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/clinic/petclinicspring/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package com.clinic.petclinicspring.services.map;
+
+import com.clinic.petclinicspring.models.Speciality;
+import com.clinic.petclinicspring.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Optional<Speciality> findById(Long id) {
+        return super.findById(id);
+    }
+}
