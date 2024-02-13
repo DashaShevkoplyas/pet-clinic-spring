25b393526c3765e7595d0008ff7af374fb13b6e7
DashaShevkoplyas <shevkoplyasdarya23@gmail.com>
2024-02-13 17:24:51 +0300

    Update Base Entity. Closes #35.

--- pet-clinic-data/src/main/java/com/clinic/petclinicspring/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinicspring/models/BaseEntity.java
@@ -1,11 +1,19 @@
 package com.clinic.petclinicspring.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
 /**
  * Base entity class that contains id field needed by all models.
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
